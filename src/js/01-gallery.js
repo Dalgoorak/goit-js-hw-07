9de0b4d68628cb9227b3b6a8ee61943800ebef29
Dalgoorak <103432002+Dalgoorak@users.noreply.github.com>
2022-09-08 22:07:09 +0300

    create HW 7

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const newGalleryItem = createGalleryItems(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", newGalleryItem);
+
+// console.log(createGalleryItems(galleryItems));
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const selectedImage = e.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`);
+
+  instance.show();
+  gallery.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  });
+});
